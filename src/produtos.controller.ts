83678f2dd5ba45903708ba2d0b8534722d41c9a1
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-07-10 09:42:59 -0300

    Modelando um Produto

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,32 +1,38 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Produto } from "./produto.model";
 
 @Controller('produtos')
 export class ProdutosController {
 
+    produtos: Produto[] = [
+        new Produto('LIV01', 'Livro de TDD e BDD na prática', 28.71),
+        new Produto('LIV02', 'Livro iniciando com flutter', 31.92),
+        new Produto('LIV03', 'Inteligência artificial como serviço', 49.90)
+    ];
+
     @Get()
-    public obterTodos(): string {
-        return 'Lista todos os produtos';
+    public obterTodos(): Produto[] {
+        return this.produtos;
     }
 
     @Get(':id')
-    public obterUm(@Param() params): string {
-        return `Retorna os dados do produto ${params.id}`;
+    public obterUm(@Param() params): Produto {
+        return this.produtos[0];
     }
 
     @Post()
-    public criar(@Body() produto): string {
-        console.log(produto);
-        return 'Produto criado';
+    public criar(@Body() produto): void {
+        produto.id = 100;
+        this.produtos.push(produto);
     }
 
     @Put()
-    public alterar(@Body() produto): string {
-        console.log(produto);
-        return 'Produto atualizado';
+    public alterar(@Body() produto): Produto {
+        return produto;
     }
 
     @Delete(':id')
-    public apagar(@Param() params): string {
-        return `Apaga o produto ${params.id}`;
+    public apagar(@Param() params): void {
+        this.produtos.pop();
     }
 }
