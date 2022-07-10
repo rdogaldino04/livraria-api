d2775a73fc1773e6ebe96e23f7e02c3c01df799f
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-07-10 16:28:23 -0300

    Criando o ProdutosService

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,38 +1,42 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { Produto } from "./produto.model";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Produto } from './produto.model';
+import { ProdutosService } from './produtos.service';
 
 @Controller('produtos')
 export class ProdutosController {
+  constructor(private produtosService: ProdutosService) {}
 
-    produtos: Produto[] = [
-        new Produto('LIV01', 'Livro de TDD e BDD na prática', 28.71),
-        new Produto('LIV02', 'Livro iniciando com flutter', 31.92),
-        new Produto('LIV03', 'Inteligência artificial como serviço', 49.90)
-    ];
+  @Get()
+  public obterTodos(): Produto[] {
+    return this.produtosService.obterTodos();
+  }
 
-    @Get()
-    public obterTodos(): Produto[] {
-        return this.produtos;
-    }
+  @Get(':id')
+  public obterUm(@Param() params): Produto {
+    return this.produtosService.obterUm(params.id);
+  }
 
-    @Get(':id')
-    public obterUm(@Param() params): Produto {
-        return this.produtos[0];
-    }
+  @Post()
+  public criar(@Body() produto: Produto): void {
+    produto.id = 100;
+    this.produtosService.criar(produto);
+  }
 
-    @Post()
-    public criar(@Body() produto): void {
-        produto.id = 100;
-        this.produtos.push(produto);
-    }
+  @Put()
+  public alterar(@Body() produto: Produto): Produto {
+    return this.produtosService.alterar(produto);
+  }
 
-    @Put()
-    public alterar(@Body() produto): Produto {
-        return produto;
-    }
-
-    @Delete(':id')
-    public apagar(@Param() params): void {
-        this.produtos.pop();
-    }
+  @Delete(':id')
+  public apagar(@Param() params): void {
+    this.produtosService.apagar(params.id);
+  }
 }
