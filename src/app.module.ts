cde022b06aaa3040bf485e5307e8e83eb6b87ad7
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-07-11 14:02:23 -0300

    configura typeorm para postegres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Livro } from './livro.model';
 import { LivrosController } from './livros.controller';
 import { LivrosService } from './livros.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '?',
+      port: 5432,
+      database: '?',
+      username: '?',
+      password:
+        '?',
+      entities: [Livro],
+      synchronize: true, // never use TRUE in production!
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
+  ],
   controllers: [AppController, LivrosController],
   providers: [AppService, LivrosService],
 })
--- src/livro.model.ts
+++ src/livro.model.ts
@@ -1,7 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Livro {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ length: 100 })
   codigo: string;
+
+  @Column({ length: 100 })
   nome: string;
+
+  @Column({ type: 'decimal', precision: 5, scale: 2, default: 0 })
   preco: number;
 
   constructor(codigo: string, nome: string, preco: number) {
